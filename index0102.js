39252f741266ae0ac7f1d0ab63c5aeff46895aa7
yeohaein <haaaein@naver.com>
2021-01-02 12:52:43 +0900

    function practice two

--- index0102.js
+++ index0102.js
@@ -1,30 +1,24 @@
 const title = document.querySelector("#title");
 
-const BASE_COLOR = "rgb(52, 73, 94)";
-const OTHER_COLOR = "#7f8c8d";
+const CLICKED_CLASS = "clicked";
+
+/*
+function handleClick() {
+    const hasClass = title.classList.contains(CLICKED_CLASS);
+
+    if (hasClass) {
+        title.classList.remove(CLICKED_CLASS);
+    } else {
+        title.classList.add(CLICKED_CLASS);
+    }
+}
+*/
 
 function handleClick() {
-    const currentColor = title.style.color;
-    if (currentColor === BASE_COLOR) {
-        title.style.color = OTHER_COLOR;
-    } else {
-        title.style.color = BASE_COLOR;
-    }
+    title.classList.toggle(CLICKED_CLASS);
 }
 
 function init() {
-    title.style.color = BASE_COLOR;
     title.addEventListener("click", handleClick);
 }
 init();
-
-function handleOffline() {
-    console.log("Bye Bye");
-}
-
-function handleOnline() {
-    console.log("Welcome Back");
-}
-
-window.addEventListener("offline", handleOffline);
-window.addEventListener("online", handleOnline);
