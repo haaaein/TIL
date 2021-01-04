f753203ef35517e583edbfb76fc5cbcf7c06463d
yeohaein <haaaein@naver.com>
2021-01-04 19:17:31 +0900

    Saving the User Name

--- clock.js
+++ clock.js
@@ -3,16 +3,17 @@ const clockContainer = document.querySelector(".js-clock"),
 
 function getTime() {
     const date = new Date();
-    const minutes = data.getMintues();
-    const hours = data.getHours();
-    const seconds = data.getSeconds();
+    const minutes = date.getMinutes();
+    const hours = date.getHours();
+    const seconds = date.getSeconds();
     clockTitle.innerText = `${
-        housrs < 10 ? `0${hours}` : hours}:${
+        hours < 10 ? `0${hours}` : hours}:${
             minutes < 10 ? `0${minutes}` : minutes
         }:${seconds < 10 ? `0${seconds}` : seconds
     }`;
 
 }
+
 function init() {
     getTime();
     setInterval(getTime, 1000);
