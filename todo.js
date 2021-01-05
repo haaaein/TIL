97be4fff624ce9c9c32f73a6b85de269222f89c4
yeohaein <haaaein@naver.com>
2021-01-05 11:46:06 +0900

    Making a To Do List part Two

--- todo.js
+++ todo.js
@@ -2,17 +2,35 @@ const toDoForm = document.querySelector(".js-toDoForm"),
     toDoInput = toDoForm.querySelector("input"),
     toDoList = document.querySelector(".js-toDoList");
 
-const TODOS_LS = 'toDos';
+const TODOS_LS = "toDos";
+
+const toDos = [];
+
+function saveToDos(){
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+    /*
+    JSON.stringify는 자바스크립트 object를 string으로 바꿔주는 역할
+    자바스크립트는 local storage에 있는 모든 데이터를 string으로 저장하기 때문
+    */
+}
 
 function paintToDo(text) {
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
-    delBtn.innerText = "❌";
     const span = document.createElement("span");
+    const newId = toDos.length + 1;
+    delBtn.innerText = "❌";
     span.innerText = text;
-    li.appendChild(span);
     li.appendChild(delBtn);
+    li.appendChild(span);
+    li.id = newId;
     toDoList.appendChild(li);
+    const toDoObj = {
+        text: text,
+        id: newId
+    };
+    toDos.push(toDoObj);
+    saveToDos();
 }
 
 function handleSubmit(event) {
@@ -23,9 +41,15 @@ function handleSubmit(event) {
 }
 
 function loadToDos() {
-    const toDos = localStorage.getItem(TODOS_LS);
-    if(toDos !== null) {
-
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if(loadedToDos !== null) {
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach(function(toDo){
+            paintToDo(toDo.text);
+        });
+        /*
+        forEach함수는 array에 담겨있는 것들 각각에 한번씩 함수를 실행시켜 주는 것.
+        */
     } 
 }
 
