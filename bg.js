d84d51fab8ca82e1c45e9b97d7be822fca721087
yeohaein <haaaein@naver.com>
2021-01-06 10:55:10 +0900

    Image Background

--- bg.js
+++ bg.js
@@ -5,7 +5,7 @@ const IMG_NUMBER = 3;
 function paintImage(imgNumber){
     const image = new Image();
     image.src = `images/${imgNumber + 1}.jpg`;
-    image.classList.add("bgIamge");
+    image.classList.add("bgImage");
     body.prepend(image);
 }
 
