a3bf753057a8371f448179d9b05c5f7292315b7d
yeohaein <haaaein@naver.com>
2021-01-19 18:57:34 +0900

    Sexy Strings

---
+++ ES6/Strings.js
@@ -0,0 +1,11 @@
+//Sexy Strings
+const sayHi = (aName = "anon") => `hello ${aName} lovely to have you`;
+
+console.log(sayHi());
+
+console.log(`hello how are you ${1000 * 39383} `);
+
+const add = (a, b) => a + b;
+
+console.log(`hello how are you ${add(6, 6)} `);
+//function도 가능!!
