9408bd20f11e2dbfaa2c2e080c444a08ca9c02c7
yeohaein <haaaein@naver.com>
2021-01-20 17:16:16 +0900

    HTML Fragments part Two

--- ES6/Strings.js
+++ ES6/Strings.js
@@ -33,3 +33,18 @@ const addWelcome = () => {
 };
 
 setTimeout(addWelcome, 2000);
+
+//map은 무엇을 return 하던지 array로 만든다!!!!
+const wrapper = document.querySelector(".wrapper");
+
+const friends = ["me", "you", "he", "she"];
+
+const list = `
+    <h1>People I love</h1>
+    <ul>
+        ${friends.map(friend => `<li>${friend}<li>`).join(" ")}
+    </ul>
+`;
+//array로 생성되기 때문에 ,가 붙는다. join " " 으로 합쳐주면 안붙음
+
+wrapper.innerHTML = list;
