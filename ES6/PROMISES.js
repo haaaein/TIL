36b872f9c73c5699db68b7360425a8eccc81b523
yeohaein <haaaein@naver.com>
2021-01-27 16:49:07 +0900

    Using Promises

--- ES6/PROMISES.js
+++ ES6/PROMISES.js
@@ -16,4 +16,17 @@ console.log("something");
  setInterval(console.log, 1000, amISexy);
  //매 초마다
 
+/**
+ * Using Promises
+ */
+
+const amISexy = new Promise((resolve, reject) => {
+    setTimeout(reject, 3000, "You are ugly"); //error
+});
+
+ amISexy
+    .then(result => console.log(result))
+    .catch(error => console.log(error));
+
+
 
