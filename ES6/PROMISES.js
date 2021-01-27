bbc2841167f5260260f64da1b32970ac76f517e5
yeohaein <haaaein@naver.com>
2021-01-27 16:36:36 +0900

    Creating Promises

--- ES6/PROMISES.js
+++ ES6/PROMISES.js
@@ -2,3 +2,18 @@
 const hello = fetch("http://google.com");
 
 console.log("something");
+
+/**
+ * Creating Promises
+ */
+
+ const amISexy = new Promise((resolve, reject) => {
+     setTimeout(resolve, 3000, "Yes you are");
+ });
+
+ console.log(amISexy);
+
+ setInterval(console.log, 1000, amISexy);
+ //매 초마다
+
+
